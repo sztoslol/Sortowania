0e698cbd2a93038d58d2bedbeccf06a3f484de1d
SztosPV <109919302+SztosPV@users.noreply.github.com>
2023-04-28 17:03:24 +0200

    Dodanie funkcji sortujacych

--- app/src/main/java/com/example/sortowania/MainActivity.kt
+++ app/src/main/java/com/example/sortowania/MainActivity.kt
@@ -22,8 +22,6 @@ class MainActivity : AppCompatActivity() {
 
         var input_ile_razy = findViewById<EditText>(R.id.input_ile_razy)
         var input_ile_elementow = findViewById<EditText>(R.id.input_ile_elementow)
-
-
     }
 }
 
@@ -37,4 +35,106 @@ fun losuj(size: Int): MutableList<Int> {
 fun calcTime(t1 : Long, t2 : Long) : Long
 {
     return t2 - t1
+}
+
+//================================Sortowanie Rabina-Karpa=====================================
+fun sortowanie_Rabin_Karp(arr: IntArray): IntArray {
+    val base = 101
+    val mod = 1000000007
+    var n = arr.size
+    var pow = 1
+    var maxVal = arr.maxOrNull()!!
+    while (maxVal / pow > 0) {
+        val count = IntArray(10)
+        for (i in arr.indices) {
+            val digit = arr[i] / pow % 10
+            count[digit]++
+        }
+        for (i in 1 until 10) {
+            count[i] += count[i - 1]
+        }
+        val temp = IntArray(n)
+        for (i in n - 1 downTo 0) {
+            val digit = arr[i] / pow % 10
+            temp[count[digit] - 1] = arr[i]
+            count[digit]--
+        }
+        for (i in arr.indices) {
+            arr[i] = temp[i]
+        }
+        pow *= 10
+    }
+    return arr
+}
+
+//==================================Sortowanie Bruteforce=======================================
+fun sortowanie_Bruteforce(arr: IntArray): IntArray {
+    val n = arr.size
+    for (i in 0 until n) {
+        for (j in i+1 until n) {
+            if (arr[i] > arr[j]) {
+                val temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
+
+//===============================Sortowanie Knutha-Morrisa-Pratta==================================
+fun kmpSort(arr: IntArray) {
+    val n = arr.size
+    val aux = IntArray(n)
+    var lo = 0
+    var hi = n - 1
+    var d = 0
+    var m = 1
+
+    while (hi >= m) {
+        m *= 10
+        d++
+    }
+
+    for (i in 1..d) {
+        val count = IntArray(10)
+
+        for (j in lo..hi) {
+            val digit = (arr[j] / m) % 10
+            count[digit]++
+        }
+
+        for (j in 1 until 10) {
+            count[j] += count[j - 1]
+        }
+
+        for (j in hi downTo lo) {
+            val digit = (arr[j] / m) % 10
+            aux[count[digit] - 1] = arr[j]
+            count[digit]--
+        }
+
+        for (j in lo..hi) {
+            arr[j] = aux[j - lo]
+        }
+
+        m /= 10
+    }
+}
+
+//===============================Sortowanie Knutha-Morrisa-Pratta==================================
+fun sortowanie_BM(arr: IntArray): IntArray {
+    val n = arr.size
+    val maxVal = arr.maxOrNull() ?: 0
+    val counts = IntArray(maxVal + 1)
+    for (x in arr) {
+        counts[x]++
+    }
+    var i = 0
+    for (x in 0..maxVal) {
+        for (j in 0 until counts[x]) {
+            arr[i++] = x
+        }
+    }
+    return arr
 }
