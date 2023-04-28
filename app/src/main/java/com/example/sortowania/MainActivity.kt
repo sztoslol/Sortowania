5fdbd3cd778e8555a3fc398bccb006a0f9c1f547
SztosPV <109919302+SztosPV@users.noreply.github.com>
2023-04-28 16:45:47 +0200

    Przypisanie elementow UI do zmiennych

--- app/src/main/java/com/example/sortowania/MainActivity.kt
+++ app/src/main/java/com/example/sortowania/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.sortowania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Przypisanie elementow UI do zmiennych
+        var wynik_rabin_karp = findViewById<TextView>(R.id.textView_sortowanie_wynik_KarpRabin)
+        var wynik_bruteforce = findViewById<TextView>(R.id.textView_sortowanie_wynik_Bruteforce)
+        var wynik_KMP = findViewById<TextView>(R.id.textView_sortowanie_wynik_KMP)
+        var wynik_BM = findViewById<TextView>(R.id.textView_sortowanie_wynik_BM)
+
+        var button_wykonaj = findViewById<Button>(R.id.button_wykonaj)
+
+        var input_ile_razy = findViewById<EditText>(R.id.input_ile_razy)
+        var input_ile_elementow = findViewById<EditText>(R.id.input_ile_elementow)
     }
 }
